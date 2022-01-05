8f43e8f0efb1976582093983c06fa43bb2967b5b
reuschj <reuschj@gmail.com>
2022-01-05 00:38:02 -0500

    Initial Commit

---
+++ Crono/CronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CronoApp.swift
+//  Crono
+//
+//  Created by Justin Reusch on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
